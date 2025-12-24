fe481f877d35bc4e230cc1e5a16d8e71fef8dd87
Elyor Djalilov <edjalilov.dev@gmail.com>
2025-12-24 14:53:57 +0500

    feat: add INavigationItem interface to common types

--- src/shared/types/common.ts
+++ src/shared/types/common.ts
@@ -7,3 +7,10 @@ export enum Locales {
 export type TFunction = (key: string) => string
 
 export type LocaleBasedData<T> = Record<Locales, T>
+
+export interface INavigationItem {
+  path: string
+  icon: React.ComponentType<{ size?: number; stroke?: number }>
+  iconFilled: React.ComponentType<{ size?: number; stroke?: number }>
+  label: string
+}
--- src/shared/types/index.ts
+++ src/shared/types/index.ts
@@ -1,2 +1,2 @@
-export type { LocaleBasedData, TFunction } from './common'
+export type { INavigationItem, LocaleBasedData, TFunction } from './common'
 export { Locales } from './common'
