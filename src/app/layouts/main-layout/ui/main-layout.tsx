a4355375fdc3f987c5956aa1e2ff3ee944f4482f
Elyor Djalilov <edjalilov.dev@gmail.com>
2025-12-24 13:39:13 +0500

    feat: add error handling UI with translations for multiple languages

--- src/app/layouts/main-layout/ui/main-layout.tsx
+++ src/app/layouts/main-layout/ui/main-layout.tsx
@@ -1,16 +1,28 @@
 import cn from './main-layout.module.css'
 
 import { AppShell } from '@mantine/core'
-import classNames from 'classnames'
+import { ASSETS } from '@shared/constants/assets'
+import { ErrorMessage } from '@shared/ui/error-message/error-message'
 import { Suspense } from 'react'
 import { ErrorBoundary } from 'react-error-boundary'
+import { useTranslation } from 'react-i18next'
 import { Outlet } from 'react-router-dom'
 
 export const MainLayout = () => {
+  const { t } = useTranslation()
+
+  const fallback = (
+    <ErrorMessage
+      title={t('errorTitle')}
+      text={t('errorSubtitle')}
+      image={ASSETS.BUG_FIXING}
+    />
+  )
+
   return (
     <AppShell>
-      <AppShell.Main className={classNames(cn.main)}>
-        <ErrorBoundary fallback={'Error...'}>
+      <AppShell.Main className={cn.main}>
+        <ErrorBoundary fallback={fallback}>
           <Suspense>
             <Outlet />
           </Suspense>
