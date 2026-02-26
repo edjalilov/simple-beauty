66cbb33ef0cf9d56b374a81547ee99bdbeb4bc2d
Elyor Djalilov <edjalilov.dev@gmail.com>
2026-02-26 15:21:30 +0500

    feat: set basename for router in WithRouter component to support deployment

--- src/app/providers/with-router.tsx
+++ src/app/providers/with-router.tsx
@@ -2,11 +2,14 @@ import { appRouter } from '../router/app-router'
 import { createBrowserRouter, Outlet, RouterProvider } from 'react-router-dom'
 
 export const WithRouter = () => {
-  const router = createBrowserRouter([
-    {
-      element: <Outlet />,
-      children: appRouter,
-    },
-  ])
+  const router = createBrowserRouter(
+    [
+      {
+        element: <Outlet />,
+        children: appRouter,
+      },
+    ],
+    { basename: import.meta.env.BASE_URL },
+  )
   return <RouterProvider router={router} />
 }
